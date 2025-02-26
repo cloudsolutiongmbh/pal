281039d2875cdcfa89c1c32b04b9a258b0dab339
cloudsolutiongmbh <84773107+cloudsolutiongmbh@users.noreply.github.com>
2025-02-26 21:48:21 +0100

    Update win10_simple.ps1

--- win10_simple.ps1
+++ win10_simple.ps1
@@ -10,7 +10,22 @@ Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module OSD -Force
 
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force   
+Import-Module OSD -Force
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
 
 #=======================================================================
 #   [OS] Params and Start-OSDCloud
