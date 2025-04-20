6db3c951051eb126386804b4f959ad0134f0e47d
cloudsolutiongmbh <84773107+cloudsolutiongmbh@users.noreply.github.com>
2025-04-20 17:33:56 +0200

    Update win10_simple.ps1

--- win10_simple.ps1
+++ win10_simple.ps1
@@ -124,10 +124,10 @@ start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F F:\autopilot.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F G:\autopilot.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F H:\autopilot.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\update.ps1
-#start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\cleanlogs.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\cleanlogs.ps1
 
 # Below a PS session for debug and testing in system context, # when not needed 
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass
+#start /wait powershell.exe -NoL -ExecutionPolicy Bypass
 
 exit 
 '@
