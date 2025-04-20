5513cd7c02d374a38e7567b7c2790b7ba3d661c6
cloudsolutiongmbh <84773107+cloudsolutiongmbh@users.noreply.github.com>
2025-04-20 11:03:01 +0200

    Update windows_update.ps1

--- windows_update.ps1
+++ windows_update.ps1
@@ -48,6 +48,18 @@ This script uses the Windows Update COM objects to install the latest cumulative
 .\UpdateOS.ps1
 #>
 
+$Title = "Windows Update"
+$host.UI.RawUI.WindowTitle = $Title
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$env:APPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Roaming"
+$env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
+$Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scripts"
+$env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
+
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Windows-Update.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
 
 [CmdletBinding()]
 Param(
